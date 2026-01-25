272548df2d23745141fb614f25d37d22734c0bf3
Ramaalomair <162565233+Ramaalomair@users.noreply.github.com>
2026-01-26 00:10:04 +0300

    fix: Use raw GitHub URL for model download

--- currency_recognition.py
+++ currency_recognition.py
@@ -9,10 +9,10 @@ import sys
 
 # متغيرات عامة
 MODEL = None
-MODEL_PATH = "SVM_.RBF.pkl"
+MODEL_PATH = "SVM_(RBF).pkl"
 
 # رابط GitHub Release
-MODEL_URL = "https://github.com/Ramaalomair/currency_api/releases/download/v1.0.0/SVM_.RBF.pkl"
+MODEL_URL = "https://github.com/Ramaalomair/currency_api/releases/download/v1.0.0/SVM_(RBF).pkl"
 
 def initialize_currency_recognition():
     """تحميل موديل SVM مرة واحدة"""
