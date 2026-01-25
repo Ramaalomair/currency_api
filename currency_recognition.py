1950184396760abe597776f6d7ef0ffdc300cf92
Ramaalomair <162565233+Ramaalomair@users.noreply.github.com>
2026-01-26 00:25:23 +0300

    Change model URL to GitHub Releases link
    Updated the model URL to point to the GitHub Releases.

--- currency_recognition.py
+++ currency_recognition.py
@@ -12,7 +12,7 @@ MODEL = None
 MODEL_PATH = "SVM_(RBF).pkl"
 
 # رابط GitHub Release
-MODEL_URL = "https://raw.githubusercontent.com/Ramaalomair/currency_api/refs/heads/main/models/currency/SVM_(RBF).pkl"
+MODEL_URL = "https://github.com/Ramaalomair/currency_api/releases/download/v1.0.0/SVM_(RBF).pkl"
 
 def initialize_currency_recognition():
     """تحميل موديل SVM مرة واحدة"""
